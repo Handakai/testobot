706746d38802e99f06c8979707007d4cd38de44f
Anton Stepantsov <stepanntosh@gmail.com>
2021-05-02 17:12:46 +0700

    init commit

---
+++ keyboards.py
@@ -0,0 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup
+
+
+GREETS_KB = ReplyKeyboardMarkup().add("Пройти тест")
+QUEST_1 = ReplyKeyboardMarkup().add("USP-S").add("DEAGLE").add("P250")
+QUEST_2 = ReplyKeyboardMarkup().add("AK-47").add("M4A4").add("P90")
+QUEST_3 = ReplyKeyboardMarkup().add("AUG").add("AWP").add("FAMAS")
+END_KB = ReplyKeyboardMarkup().add("Получить скин")
+END_KEYB = ReplyKeyboardMarkup().add("Информация")
